41a28e787972366a358c09f282680f5a329b3f41
dayanecode <dayanecode@gmail.com>
2023-02-05 11:19:02 -0300

    textos alterados

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -3,11 +3,11 @@ import './GameOver.css'
 const GameOver = ({retry, score }) => {
   return (
     <div>
-        <h1>Fim do jogo</h1>
+        <h1>Fim do jogo!</h1>
         <h2>
           A sua pontuação foi: <span>{score}</span>
         </h2>
-        <button onClick={retry}>Resetar Jogo</button>
+        <button onClick={retry}>Reiniciar</button>
     </div>
   )
 }
